9e570d4385b4811a4fe4d9abc7885be5c4b2709f
aditya mehto <adityamehto19@gmail.com>
2023-07-20 13:41:03 +0530

    Initial commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function Navbar  () {
+  return (
+<nav className='container'>
+    <div className="logo">
+       <img src="/images/brand_logo.png" alt="brandlogo Imag" />
+    </div>
+
+    
+    <ul>
+      <li href="#">Menu</li>
+      <li href="#">Location</li>
+      <li href="#">About</li>
+      <li href="#">Contact</li>
+    </ul>
+
+    <button>Login</button>
+
+</nav>
+  )
+}
+
+
+
+// import React from 'react'
+
+// export default function Navbar() {
+//   return (
+//     <div>navbar</div>
+//   )
+// }
